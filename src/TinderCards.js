03afb0fb03c9e9c95891dda26dfa1f680c19935f
Daniel Hong <hongchangyeon@gmail.com>
2020-12-14 18:59:50 -0600

    tinder card box rendered

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import TinderCard from 'react-tinder-card';
+import './TinderCards.css';
 
 function TinderCards() {
     const [people,setPeople] = useState([
         {
             name: "Steve Jobs",
-            url: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg"
+            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5b8576db31358e0429c734e3%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D211%26cropX2%3D2381%26cropY1%3D900%26cropY2%3D3072"
         },
 
         {
