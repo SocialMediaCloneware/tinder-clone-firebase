46a6bb9eb4b7008e3d5e64f3551141c5fa76d416
Daniel Hong <hongchangyeon@gmail.com>
2020-12-14 18:31:25 -0600

    router setup for homepage and chat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Router>  
-      {/* Header */}
       <Header />
+      <Router>  
+        <Switch>
+
+          <Route path="/chat">
+          <h1>I am the chat page</h1>
+          </Route>
+
+          <Route path="/">
+            <h1>I am the homepage</h1>
+          </Route>
+
+        </Switch>
+      {/* Header */}
+      {/* <Header /> */}
 
       
 
