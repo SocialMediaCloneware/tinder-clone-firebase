74893e4328bf38015c9095535601b8bb552345cf
Daniel Hong <hongchangyeon@gmail.com>
2020-12-14 18:34:02 -0600

    Tinder cards installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import TinderCards from './TinderCards';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      {/* Header */}
       <Header />
+
+      {/* <Router>      */}
       <Router>  
         <Switch>
 
@@ -19,8 +23,7 @@ function App() {
           </Route>
 
         </Switch>
-      {/* Header */}
-      {/* <Header /> */}
+      
 
       
 
